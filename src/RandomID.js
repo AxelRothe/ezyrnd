02d1fbac1015d8ea4242ccceafa85ce8fff91746
axelrothe <hello@axelrothe.com>
2022-02-16 17:51:23 +0100

    removes cryptojs, implemented xxhash and better compatibility of resulting strings

--- src/RandomID.js
+++ src/RandomID.js
@@ -3,43 +3,70 @@ Random ID Electron Version
 Author: Axel Rothe (c) 2021 All Rights Reserved
  */
 
-const CryptoJS = require("crypto-js");
+const { XXHash64, XXHash32, XXHash3, XXHash128 } = require("xxhash-addon");
+const {StringDecoder} = require("string_decoder");
 
 /**
  * RandomID - The randomized and 100% collision free ID Generator
- * Version: Electron - Bzzzzt
  */
 class RandomID {
+    static key = 12345678;
+    static hashStyle = "xxhash128";
+
     /**
      * Create a new RandomID instance
      * @param key {String} (optional) Setting a Key creates a unique instance of RandomID, that you can use to generate unique sets
      */
-    constructor(key = "randomidkey") {
+    constructor() {
         /* build seed from timestamp and a random string - this ensures that each time RandomID is instanced it creates a pseudo unique seed and can't accidentally override each other */
         this.seed = (Date.now() + this.randomString(12)).toString();
         /* the counter is used to keep track of how often this instance was called */
         this.counter = 0;
-        /* Key is used to create unique RandomID instances */
-        this.key = key;
-
-        // scriptLoader.load("/crypto-js/crypto-js.js");
     }
 
     /**
-     * Generates a system unique ID
+     * Hashes a File or retrieves it's hash from the Indexer
      *
-     * @return {String} the ID
+     * @param {string?} hashStyle xxhash3, xxhash32, xxhash63, xxhash128 default: xxhash128
+     * @returns {String} returns the hash or a falsey value if the file is unavailable
      */
-    generate() {
-        /* Tick up counter */
-        this.counter++;
-        /* We enforce compression of data to a uniform output with AES
-         *  The key we use is irrelevant, but it needs to be permanent
-         */
-        let cryptoHash = CryptoJS.AES.encrypt(this.seed + this.counter, this.key);
-        return btoa(cryptoHash.toString()).replaceAll("/", "x").replaceAll("+", "y");
+    generate(hashStyle = RandomID.hashStyle) {
+        this.counter++
+        return this.buildHash(Date.now()+this.seed+this.counter,"xxhash128");
     }
 
+    buildHash(string, hashStyle = RandomID.hashStyle){
+	    let hash;
+
+	    switch (hashStyle) {
+		    case "xxhash128":
+			    hash = new XXHash128(RandomID.key);
+			    break;
+		    case "xxhash64":
+			    hash = new XXHash64(RandomID.key);
+			    break;
+		    case "xxhash32":
+			    hash = new XXHash32(RandomID.key);
+			    break;
+		    case "xxhash3":
+			    hash = new XXHash3(RandomID.key);
+			    break;
+		    default:
+			    hash = new XXHash64(RandomID.key);
+	    }
+
+	    hash.update(Buffer.from(string));
+	    let digest = hash.digest();
+	    const decoder = new StringDecoder("base64");
+	    return decoder.write(digest);
+    }
+
+    /**
+     * Generates a random string of a desired length
+     *
+     * @param {number} length the number of chars to be generated
+     * @return {string} string of x length possible characters a-z,A-Z,0-9
+     */
     randomString(length) {
         const chars = [
             "0",
